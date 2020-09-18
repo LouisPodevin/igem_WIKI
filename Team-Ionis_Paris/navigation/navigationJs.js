fca6a94f63381dbf0ed8adeaf800980b89dbac71
Tran Tâm-Tanguy <thanh-tam-tangu.tran@epita.fr>
2020-09-18 11:14:28 +0200

    feat(nav): Add nav color

--- Team-Ionis_Paris/navigation/navigationJs.js
+++ Team-Ionis_Paris/navigation/navigationJs.js
@@ -1,5 +1,8 @@
 // @ts-check
 
+// @ts-ignore
+const rootUrl = window.ROOT_URL
+
 /** @type {Array<
  * {
  *  title: string
@@ -7,20 +10,19 @@
  * (
  *  {
  *  link: string,
+ *  match?: string | Array<string>
  * } | {
  *  childrens: Array<{
  *      title: string,
- *      link: string
+ *      link: string,
+ *      match?: string | Array<string>
  *  }>})
  * >} */
-
-// @ts-ignore
-const rootUrl = window.ROOT_URL
-
 const menu = [
     {
         title: "Home",
         link: rootUrl + "/",
+        match: [rootUrl, `${rootUrl}/`],
     },
     {
         title: "Team",
@@ -77,6 +79,45 @@ const menu = [
     },
 ]
 
+const actualPos = window.location.pathname
+
+/** @param {string | Array<string>} url */
+const matchUrl = url => {
+    /** @type {(elm: string | Array<string>) => elm is Array<string>} */
+    const isArray = elm => {
+        return Array.isArray(elm)
+    }
+
+    if (isArray(url)) {
+        return url.reduce((prev, curr) => {
+            if (prev) {
+                return true
+            }
+
+            const regexElm = new RegExp(curr)
+            return actualPos.search(regexElm) !== -1
+        }, false)
+    }
+
+    const regexElm = new RegExp(url)
+    return actualPos.search(regexElm) !== -1
+}
+
+/** @type {Array<boolean | Array<boolean>>} */
+const active = Array(menu.length)
+    .fill(false)
+    .map((elm, pos) => {
+        /**@type {any} */
+        const link = menu[pos]
+        if (link.childrens) {
+            return link.childrens.map(elm2 => {
+                return !!elm2.match && matchUrl(elm2.match)
+            })
+        } else {
+            return !!link.match && matchUrl(link.match)
+        }
+    })
+
 const initNav = async () => {
     // @ts-ignore
     await import(window.LINKS.vueJs.url)
@@ -87,8 +128,17 @@ const initNav = async () => {
         data() {
             return {
                 menu: menu.map((value, index) => ({ ...value, index })),
-                openedMenu: Array(menu.length).fill(false),
-                active: 0,
+                openedMenu: Array(menu.length)
+                    .fill(false)
+                    .map((_, pos) => {
+                        const activeElm = active[pos]
+                        if (Array.isArray(activeElm)) {
+                            return activeElm.reduce((prev, curr) => {
+                                return prev || curr
+                            }, false)
+                        }
+                    }),
+                active: active,
             }
         },
         methods: {
