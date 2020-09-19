e08c7ca88b82be5f42c737a6242dc28bcc699e00
Tran Tâm-Tanguy <thanh-tam-tangu.tran@epita.fr>
2020-09-19 12:17:45 +0200

    fix: Fix merge errros

--- Team-Ionis_Paris/navigation/navigationJs.js
+++ Team-Ionis_Paris/navigation/navigationJs.js
@@ -32,18 +32,11 @@ const menu = [
     {
         title: "Project",
         childrens: [
-<<<<<<< HEAD
-            { title: "Description", link: rootUrl + "/Project/Description", match: `^${rootUrl}/Project/Description$`, },
-
-            { title: "Test1", link: "jjj" },
-            { title: "Toto", link: rootUrl + "/toto/", match: `^${rootUrl}/toto/?$` },
-=======
             {
                 title: "Description",
                 link: rootUrl + "/Project/Description",
                 match: `^${rootUrl}/Project/Description$`,
             },
->>>>>>> 3c978589c06c26e64b0e2f70abbac80a04177959
         ],
     },
     {
