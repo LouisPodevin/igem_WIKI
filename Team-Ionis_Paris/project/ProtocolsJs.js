e708211656ac2d287f34705cf2fca0d7b25623e7
Tran Tâm-Tanguy <thanh-tam-tangu.tran@epita.fr>
2020-10-25 18:17:59 +0100

    feat: Add protocol js

---
+++ Team-Ionis_Paris/project/ProtocolsJs.js
@@ -0,0 +1,111 @@
+const files = {
+    "General protocols": [
+        {
+            file: "Absorbance reading with a spectrophotometer",
+            link: "https://2020.igem.org/wiki/images/4/4c/T--Ionis_Paris--Absorbance_reading.pdf",
+        },
+        {
+            file: "Preparation of calcium competent Escherichia coli and heat-shock transformation",
+            link: "https://2020.igem.org/wiki/images/2/2e/T--Ionis_Paris--Preparation_of_calcium.pdf",
+        },
+        {
+            file: "Resuspension of dry gBlocks Gene Fragments",
+            link: "https://2020.igem.org/wiki/images/4/43/T--Ionis_Paris--Resuspension_dry_gBlocks.pdf",
+        },
+        {
+            file: "LB agar preparation and plating",
+            link: "https://2020.igem.org/wiki/images/4/47/T--Ionis_Paris--LB_agar_preparation.pdf",
+        },
+    ],
+    "Polymer Chain reaction (PCR) protocols": [
+        {
+            file: "PCR OneTaq DNA Polymerase",
+            link: "https://2020.igem.org/wiki/images/5/53/T--Ionis_Paris--PCR_OneTaq_DNA.pdf",
+        },
+        {
+            file: "Protocol for OneTaq Hot Start Quick-Load 2X Master Mix with Standard Buffer",
+            link: "https://2020.igem.org/wiki/images/0/07/T--Ionis_Paris--Protocol_for_OneTaq.pdf",
+        },
+        {
+            file: "Protocol for Q5 High-Fidelity 2X Master Mix",
+            link: "https://2020.igem.org/wiki/images/9/98/T--Ionis_Paris--Protocol_for_Q5.pdf",
+        },
+        { file: "Colony PCR", link: "https://2020.igem.org/wiki/images/d/d1/T--Ionis_Paris--Colony_PCR.pdf" },
+        { file: "Gel Migration", link: "https://2020.igem.org/wiki/images/6/61/T--Ionis_Paris--Gel_Migration.pdf" },
+        { file: "Q5 hotsart", link: "https://2020.igem.org/wiki/images/9/9b/T--Ionis_Paris--PCR_Q5_Hot_Start.pdf" },
+    ],
+
+    "Purification protocols": [
+        {
+            file: "DNA Gel Extraction Kit",
+            link: "https://2020.igem.org/wiki/images/0/08/T--Ionis_Paris--DNA_Gel_Extraction.pdf",
+        },
+        {
+            file: "Monarch DNA PCR &DNA extraction Kit",
+            link: "https://2020.igem.org/wiki/images/3/37/T--Ionis_Paris--PCR_DNA_Cleanup.pdf",
+        },
+        { file: "DNA Miniprep Kit", link: "https://2020.igem.org/wiki/images/2/24/T--Ionis_Paris--DNA_Miniprep.pdf" },
+    ],
+
+    "Optimization protocols": [
+        { file: "DpnI", link: "https://2020.igem.org/wiki/images/6/6a/T--Ionis_Paris--DpnI.pdf" },
+        {
+            file: "Shrimp Alkaline Phosphatase (rSAP)",
+            link: "https://2020.igem.org/wiki/images/7/78/T--Ionis_Paris--Shrimp_Alkaline_Phosphatase.pdf",
+        },
+    ],
+
+    "Cloning protocols": [
+        {
+            file: "Hifi DNA Assembly Protocol",
+            link: "https://2020.igem.org/wiki/images/a/a6/T--Ionis_Paris--Hifi_DNA_Assembly_protocol.pdf",
+        },
+        {
+            file: "HiFi DNA Assembly Chemical Transformation Protocol",
+            link: "https://2020.igem.org/wiki/images/7/76/T--Ionis_Paris--HiFi_DNA_Assembly_Chemical.pdf",
+        },
+        {
+            file: "Restriction enzyme digestion",
+            link: "https://2020.igem.org/wiki/images/e/e8/T--Ionis_Paris--Restriction_enzyme_digestion.pdf",
+        },
+        {
+            file: "Ligation protocol with Hi-T4 DNA Ligase",
+            link: "https://2020.igem.org/wiki/images/a/a2/T--Ionis_Paris--Ligation_protocol.pdf",
+        },
+        {
+            file: "Ligation Transformation Protocol",
+            link: "https://2020.igem.org/wiki/images/0/00/T--Ionis_Paris--Ligation_Transformation.pdf",
+        },
+    ],
+
+    "Verification protocols": [
+        {
+            file: "Extraction, purification (Ni-TED 150 Packed Columns) and validation (SDS) of McpM production",
+            link: "https://2020.igem.org/wiki/images/7/77/T--Ionis_Paris--Extraction.pdf",
+        },
+        {
+            file: "Bacterial Immunofluorescence protocol",
+            link: "https://2020.igem.org/wiki/images/f/fe/T--Ionis_Paris--Bacterial_Immunofluorescence.pdf",
+        },
+        {
+            file: "Agar diffusion assay",
+            link: "https://2020.igem.org/wiki/images/a/a6/T--Ionis_Paris--Agar_diffusion.pdf",
+        },
+        {
+            file: "Cleavage with TEV protease",
+            link: "https://2020.igem.org/wiki/images/f/f8/T--Ionis_Paris--Cleavage_with_TEV.pdf",
+        },
+    ],
+
+    "Shelf Life Determination": [
+        { file: "Lyophilization", link: "https://2020.igem.org/wiki/images/a/ac/T--Ionis_Paris--Lyophilization.pdf" },
+        {
+            file: "CFU Determination",
+            link: "https://2020.igem.org/wiki/images/3/3d/T--Ionis_Paris--CFU_Determination.pdf",
+        },
+        {
+            file: "Accelerated Shelf Life Testing Method",
+            link: "https://2020.igem.org/wiki/images/1/10/T--Ionis_Paris--Accelerated_Shelf-Life.pdf",
+        },
+    ],
+}
